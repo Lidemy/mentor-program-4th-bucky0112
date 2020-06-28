01a9ecdec0c06d755f09f470b517935115010f1e
bucky0112 <bucky0112@gmail.com>
2020-06-28 21:52:28 +0800

    week2作業完成

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,17 @@
 function join(arr, concatStr) {
-  
+    let str = '';
+    arr.forEach((item,i)=> {
+        str += arr[i] + concatStr
+    })
+    return str
 }
 
 function repeat(str, times) {
-  
+    let string = '';
+    for(let i=0; i<times; i++) {
+        string += str
+    }
+    return string
 }
 
 console.log(join(['a'], '!'));
